fb7f48ae979ff982b8593612938ef5939ecba9c0
Subhamstar <mr.subhamdas999@gmail.com>
2025-07-05 22:06:15 +0530

    Scope Chain

---
+++ lecture1/scopeChain.js
@@ -0,0 +1,24 @@
+// function a(){
+//     console.log(b);
+    
+// }
+// let b=10;
+// a(); 
+function a(){
+    c();
+    function c(){
+        console.log(b);
+    }
+
+}
+var b=10;
+a();  //scope : where you can access a variable
+
+
+// Global exection context is created when the javascript engine starts executing the code.
+        //  |memory|Code|
+        //  |      |    |
+        //  |      |    |
+        //  |      |    |
+        //  |      |    |
+        //  |      |    |
--- lecture1/shortest.js
+++ lecture1/shortest.js
@@ -1,3 +1,34 @@
  this
  window
 //  when all javascript page is null then js engine make global execution context that have this,window like keywords
+
+var a=10;
+function b(){
+    var x=10;
+}
+console.log(a); // 10
+console.log(b); // [Function: b]
+console.log(this); // { a: 10, b: [Function: b] }
+console.log(window); // { a: 10, b: [Function: b] }
+// console.log(this===window); // true  
+// console.log(this===globalThis); // true
+// console.log(this===global); // true
+// console.log(this===self); // true
+// console.log(this===globalThis); // true
+console,log(window.a); // 10
+console.log(window.b); // [Function: b]
+
+
+
+// javascript is looslly typed language
+// javascript is dynamic typed language
+
+var a;
+console.log(a); // undefined
+a=10;
+console.log(a); // 10
+a="hello";
+console.log(a); // hello  
+
+
+// javascript is single threaded language
