5a3f996a76418436810c69d93e5c8e64c62452b8
Subhamstar <mr.subhamdas999@gmail.com>
2025-07-09 19:36:18 +0530

    annonomous function

---
+++ lecture1/anonymousfunc.js
@@ -0,0 +1,35 @@
+// 1.what is Anonymous Function ?
+// a function with out  a name it salso called anonymous function 
+
+// 2.what are first class function ?
+
+
+
+//function statement
+// a(); // this will call the function a and print "hello from a"
+// function a(){
+//     console.log("hello from a");
+// }
+// a();
+
+//function expression
+// b();this is called hoisting, it will give error because b is not defined yet
+// var b=function(){
+//     console.log("hello from b");
+// }
+// b();
+
+// //Annnymos Function
+// function(){
+
+// }
+
+// Difference betwwn Parameter and argumnt
+// var b=function(param1,param2){ // param1 and param2 is called is parameter
+//     console.log("heelo");
+// }
+// a(1,2); //this is called 
+
+
+
+// Arraow function
