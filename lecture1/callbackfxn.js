a5f824f0eabe69a00d3e896f0061894d53deb569
Subhamstar <mr.subhamdas999@gmail.com>
2025-07-19 21:42:03 +0530

    set time out practice

--- lecture1/callbackfxn.js
+++ lecture1/callbackfxn.js
@@ -1,11 +1,24 @@
-setTimeout(function (){
-    console.log("timer");
-},5000);
-function x(y){
-    console.log("x");
-    y();
-}
-x(function y(){
-    console.log("y");
-})
+// setTimeout(function (){
+//     console.log("timer");
+// },5000);
+// function x(y){
+//     console.log("x");
+//     y();
+// }
+// x(function y(){
+//     console.log("y");
+// })
 // with thi callback function and setTimeout function we can achive javascript is asynchronous function
+
+
+
+function x(){
+    // var i=10;
+    for(var i=1;i<10;i++){
+    setTimeout(function(){
+        console.log(i);// here i is closers
+    },3000)
+    console.log("Hello");
+}
+}
+x();
