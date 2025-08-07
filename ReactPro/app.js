7ab79939838786ff741ad4011576283e80f8f193
Subhamstar <mr.subhamdas999@gmail.com>
2025-08-07 22:18:28 +0530

    interview by MAF

--- ReactPro/app.js
+++ ReactPro/app.js
@@ -1,21 +1,29 @@
-    import React from "react";
-    import ReactDOM from "react-dom/client"
-    const heading =React.createElement(
-        "h1",
-        {  // props 
-            id:"title"
-        },
-        "SincosTani🪴  :--1  from parcer "
-    );
-    const heading1 =React.createElement(
-        "h2",
-        {  // props 
-            id:"title"
-        },
-        "SincosTani🪴  :--2 from parcer"
-    );
-    const container =React.createElement("div",{
-        id:"container"
-    },[heading,heading1])
-    const root=ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+    // import React from "react";
+    // import ReactDOM from "react-dom/client"
+    // const heading =React.createElement(
+    //     "h1",
+    //     {  // props 
+    //         id:"title"
+    //     },
+    //     "SincosTani🪴  :--1  from parcer "
+    // );
+    // const heading1 =React.createElement(
+    //     "h2",
+    //     {  // props 
+    //         id:"title"
+    //     },
+    //     "SincosTani🪴  :--2 from parcer"
+    // );
+    // const container =React.createElement("div",{
+    //     id:"container"
+    // },[heading,heading1])
+    // const root=ReactDOM.createRoot(document.getElementById("root"));
+    // root.render(heading);
+
+
+    var a="subh";
+    var b="subh";
+    console.log(a==b);
+    console.log(a.equals(b));
+    console.log("ab"=="ab");
+    console.log("ab".equals("b"));
---
+++ interviewMAF/question1.js
@@ -0,0 +1,8 @@
+    var a="subh";
+    var b="subh";
+    console.log(a==b);
+    console.log(a.equals(b));
+    console.log("ab"=="ab");
+    console.log("ab".equals("b"));
+
+    
