e0796f35d04c8c80fb38681a5ea1a1f08c858dd4
Subhamstar <mr.subhamdas999@gmail.com>
2025-08-07 22:23:38 +0530

    MAF

--- interviewMAF/question1.js
+++ interviewMAF/question1.js
@@ -1,8 +1,8 @@
     var a="subh";
     var b="subh";
-    console.log(a==b);
-    console.log(a.equals(b));
-    console.log("ab"=="ab");
-    console.log("ab".equals("b"));
+    console.log(a==b);   //true
+    console.log(a.equals(b));  //TypeError: a.equals is not a function
+    console.log("ab"=="ab"); //true
+    console.log("ab".equals("b"));  //TypeError 🥹😘🪴🌳
 
-    
+    // 
