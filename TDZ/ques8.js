4db9992be133e0f72edfd2ecc94e2b78cd3c8987
Subhamstar <mr.subhamdas999@gmail.com>
2025-08-09 16:55:28 +0530

    rev each word

---
+++ TDZ/ques8.js
@@ -0,0 +1,6 @@
+var str="sare jahase accha hindusthan humara";
+// var saveStr=str.split(" ")
+// .map(function(word){
+//     return word.split("").reverse().join("");
+// })
+console.log(str.split("").reverse().join(""));
