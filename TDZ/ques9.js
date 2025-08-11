f3336e3bb3b45097ed1ddb258265e5e2d3cff650
Subhamstar <mr.subhamdas999@gmail.com>
2025-08-11 23:10:47 +0530

    quiz

---
+++ TDZ/ques9.js
@@ -0,0 +1,14 @@
+console.log(12==13);
+
+console.log(12=="12"); //it can check only value but donot check type 
+console.log(12==="12")
+
+console.log(12!=12);
+console.log(12!=="12");
+
+
+console.log(0.1+0.2 !== 0.3);  //store double presision
+
+console.log([]== ![]);    // ![]js understand that [] is empty so return true ans for ! operator becomes false and fist one thins that this is ""or 0 so  
+//     "=="    -> it compaires values after performing type conversion if needed
+//     "==="   -> it checks both value and type without converting
