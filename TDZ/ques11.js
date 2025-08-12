ae4664e596514a0fc75ba9b0b8195033f5600b42
Subhamstar <mr.subhamdas999@gmail.com>
2025-08-12 21:51:33 +0530

    added

--- TDZ/ques11.js
+++ TDZ/ques11.js
@@ -1,12 +1,23 @@
- var obj = {
-    name:  "vivek",
-    getName: function(){
-    console.log(this.name);
-  }
+//  var obj = {
+//     name:  "vivek",
+//     getName: function(){
+//     console.log(this.name);
+//   }
      
+// }
+       
+// var getName = obj.getName;
+       
+// var obj2 = {name:"akshay", getName };
+// obj2.getName();   
+
+var obj1 = {
+    address : "Mumbai,India",
+    getAddress: function(){
+    console.log(this.address); 
+  }
 }
-       
-var getName = obj.getName;
-       
-var obj2 = {name:"akshay", getName };
-obj2.getName();   
+   
+var getAddress = obj1.getAddress;
+var obj2 = {name:"akshay"};
+obj2.getAddress();    
