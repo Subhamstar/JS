ec795f16700e92b04b0f84660ca2601bc8629f26
Subhamstar <mr.subhamdas999@gmail.com>
2025-08-18 22:50:14 +0530

    debounce

---
+++ TDZ/ques15.js
@@ -0,0 +1,10 @@
+function debounce(func, delay) {
+  let timer;
+  return function(...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => func.apply(this, args), delay);
+  };
+}
+
+const log = debounce(() => console.log("Typing..."), 1000);
+log(); log(); log();
