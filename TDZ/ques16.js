f65d8e4286b81e387e715907b0e1504a881d39d9
Subhamstar <mr.subhamdas999@gmail.com>
2025-08-18 23:05:09 +0530

    heigher order function

---
+++ TDZ/ques16.js
@@ -0,0 +1,12 @@
+// higher order function 
+// In JavaScript, a higher-order function is a function that either takes another function as an argument,
+// returns a function, or does both. This makes JavaScript very flexible because functions are treated 
+// as first-class citizens.
+function multiplier(x) {
+  return function(y) {
+    return x * y;
+  };
+}
+
+const double = multiplier(2);
+console.log(double(5));  
