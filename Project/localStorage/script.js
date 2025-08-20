4a7f0f5a7f552571cdc5cb298fb53f8749d5bd65
Subhamstar <mr.subhamdas999@gmail.com>
2025-08-20 23:59:13 +0530

    localstorage

--- Project/localStorage/script.js
+++ Project/localStorage/script.js
@@ -1,13 +1,18 @@
 const btn=document.getElementById("btn");
 const username=document.getElementById("username");
+const userage=document.getElementById("userage");
 const user=document.getElementById("u-name");
+const age=document.getElementById("u-age");
 btn.addEventListener("click",()=>{
     const value=username.value;
     localStorage.setItem("name",value);
+    localStorage.setItem("age",userage.value);
     location.reload();
 })
 
 window.addEventListener("load",()=>{
     const value=localStorage.getItem("name");
+    const uage=localStorage.getItem("age");
     user.innerText=value; 
+    age.innerText=uage;
 })
