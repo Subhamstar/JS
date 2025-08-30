8841618b23daa9f59445c1294c970ccf1d1569f7
Subhamstar <mr.subhamdas999@gmail.com>
2025-08-30 19:15:24 +0530

    event bubbling

--- revision/eventBubbling.js
+++ revision/eventBubbling.js
@@ -1,6 +1,11 @@
 document.getElementById("parent").addEventListener("click",()=>{
     console.log("Parent Clicked");
 })
+
+// “Event bubbling is how events propagate in the DOM. When an event occurs on a child element, 
+// it first runs its own event handler and then moves up to its parent, grandparent, 
+// and so on until it reaches the document root.
+
 document.getElementById("child").addEventListener("click",()=>{
     console.log("Child Clicked");
 })
