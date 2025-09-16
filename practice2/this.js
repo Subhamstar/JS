4cb96eb902b6b32b471a9bbc00e630ab225f51b1
Subhamstar <mr.subhamdas999@gmail.com>
2025-09-16 23:57:46 +0530

    this

--- practice2/this.js
+++ practice2/this.js
@@ -30,3 +30,9 @@ const user = { name: "Subham" };
 greet.call(user);  // Subham
 greet.apply(user); // Subham
 greet.bind(user)(); // Subham
+
+
+
+// In global scope → this refers to the global object (window in browsers).
+// Inside an object method → this refers to that object.
+// In arrow functions → this is lexically bound (takes value from surrounding scope).
